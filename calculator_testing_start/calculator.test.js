ec71c7d6a5763cf17811e02b9a1d28a6edd7c204
Thouhid Khan Majles <thouhidkhanmajles@gmail.com>
2022-06-01 10:00:23 +0100

    Tests Added

--- calculator_testing_start/calculator.test.js
+++ calculator_testing_start/calculator.test.js
@@ -10,46 +10,110 @@ const {
 
 describe('sum', () => { 
   
-  test('can add two small positive numbers', () => {
-    expected = 5;
-    actual = sum(2, 3);
+  test('We can add numbers', () => {
+    expected = 13;
+    actual = sum(6, 7);
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
-    
-  });
 
-  test.skip('can add two negative numbers', () => {
-    
+  test('We can add negative numbers', () => {
+    expected = -40;
+    actual = sum(-30, -10);
+    expect(actual).toBe(expected);
   });
-
-  test.skip('can add zero', () => {
-    
-  });
-
 });
 
 describe('subtract', () => {
 
+  test('We can subtract positive numbers', () => {
+    expected = 5;
+    actual = subtract(10, 5);
+    expect(actual).toBe(expected);
+  })
+
+  test('We can subtract negative numbers', () => {
+    expected = 20;
+    actual = subtract(-30, -50);
+    expect(actual).toBe(expected);
+  })
 });
 
 describe('multiply', () => {
 
+  test('We can multiply positive numbers', () => {
+    expected = 20;
+    actual = multiply(10, 2);
+    expect(actual).toBe(expected);
+  })
+
+  test('We can multiply negative numbers', () => {
+    expected = 35;
+    actual = multiply(-5, -7);
+    expect(actual).toBe(expected);
+  })
 });
 
 describe('divide', () => {
+  
+
+  test('We can divide positive numbers', () => {
+    expected = 41;
+    actual = divide(123, 3);
+    expect(actual).toBe(expected);
+  })
+
+  test('We can divide negative numbers', () => {
+    expected = 50;
+    actual = divide(-200, -4);
+    expect(actual).toBe(expected);
+  })
 
 });
 
 describe('modulus', () => {
 
+test('We can find the modulus of two numbers', () => {
+    expected = 0;
+    actual = modulus(10,2);
+    expect(actual).toBe(expected);
+  })
+
+  test('We can find the modulus of two numbers', () => {
+    expected = 15;
+    actual = multiply(75, 30);
+    expect(actual).toBe(expected);
+  })
+
 });
 
 describe('even', () => {
 
+  test('We can check whether a number is even', () => {
+    expected = true;
+    actual = even(14);
+    expect(actual).toBe(expected);
+  })
+  test('We can check whether a number is even', () => {
+    expected = true;
+    actual = even(2);
+    expect(actual).toBe(expected);
+  })
+
 });
 
+
 describe('odd', () => {
+
+  test('We can check whether a number is odd', () => {
+    expected = true;
+    actual = odd(7);
+    expect(actual).toBe(expected);
+  })
+  test('We can check whether a number is odd', () => {
+    expected = true;
+    actual = odd(5);
+    expect(actual).toBe(expected);
+  })
 
 });
